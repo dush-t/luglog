dc66e56357598507e8738a91285f9a2d57f9f64e
Dushyant Yadav <dushyant9309@gmail.com>
2019-09-23 23:20:45 +0530

    Integrate mobile sms sending functionality

---
+++ src/utils/sms.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+const sendSMS = (number, body) => {
+    const postData = {
+        "sender": "SOCKET",
+        "route": "4",
+        "country": "91",
+        "sms": [{
+            "message": body,
+            "to": [
+                number.toString()
+            ]
+        }]
+    }
+    
+    axios.post('https://api.msg91.com/api/v2/sendsms?country=91', postData, {
+        headers: {
+            "authkey": process.env.SMS_AUTHKEY,
+            "Content-Type": 'application/json'
+        }
+    })
+}
+
+module.exports = {
+    sendSMS
+}
