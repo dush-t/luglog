9ab60685b1bffcfe49f2dde0d6e21d2aa22e6e10
Dushyant Yadav <dushyant9309@gmail.com>
2019-10-10 22:57:49 +0530

    please

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://mongo:27017/luglog', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://dushyant:tlothwby0107@cluster0-j5uti.mongodb.net/luglog?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
