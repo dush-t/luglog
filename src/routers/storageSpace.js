53cda26326864ee766645eb81d295d721d22715d
Dushyant Yadav <dushyant9309@gmail.com>
2019-10-10 21:49:50 +0530

    whatthefuck.mp3

--- src/routers/storageSpace.js
+++ src/routers/storageSpace.js
@@ -8,7 +8,24 @@ const Image = require('../models/image');
 const auth = require('../middleware/auth');
 const adminAccess = require('../middleware/adminAccess');
 
-const imageUpload = require('../utils/imageUpload');
+const multer = require('multer');
+
+
+const imageUpload = multer({                 // No dest parameter provided because we
+    limits: {                           // do not want to save the image in the 
+        fileSize: 1000000               // filesystem. We wanna access the binary
+    },                                  // data in the router function.
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please provide a jpg, jpeg or png file'));
+        }
+
+        cb(undefined, true);
+    }
+
+})
+
+// const imageUpload = require('../utils/imageUpload');
 
 const router = new express.Router();
 
