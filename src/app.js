cda38226bce5c435786ce7f7367d58adcdd32b82
Dushyant Yadav <dushyant9309@gmail.com>
2019-10-11 18:46:29 +0530

    Bugfix

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const multer = require('multer');
+// const multer = require('multer');
 const path = require('path');
 const hbs = require('hbs')
 
