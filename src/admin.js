8e2e19308d5f9d315d602810d6d85cbf5788988e
Dushyant Yadav <dushyant9309@gmail.com>
2019-10-28 13:58:51 +0530

    Customize admin panel

--- src/admin.js
+++ src/admin.js
@@ -6,13 +6,57 @@ const Area = require('./models/area');
 const Booking = require('./models/booking');
 const StorageSpace = require('./models/storageSpace');
 const Transaction = require('./models/transaction');
-const Image = require('./models/image');
 
 // SETUP ADMIN PANEL
 AdminBro.registerAdapter(require('admin-bro-mongoose'))
 
+const visibleOnlyInDetail = { list: false, filter: false, show: true, edit: true }
+const alwaysVisible = { list: true, filter: true, show: true, edit: true }
+const invisible = { list: false, filter: false, show: false, edit: false }
+
+const userResource = {
+    resource: User,
+    options: {
+        properties: {
+            name: {isVisible: alwaysVisible},
+            email: {isVisible: alwaysVisible},
+            mobile_number: {isVisible: alwaysVisible},
+            bookings: {isVisible: alwaysVisible},
+            uniqueId: {isVisible: visibleOnlyInDetail},
+            forgotPasswordOTP: {isVisible: visibleOnlyInDetail},
+            is_luggage_stored: {isVisible: invisible},
+            password: {isVisible: invisible}
+        }
+    }
+}
+
+const storageSpaceResource = {
+    resource: StorageSpace,
+    options: {
+        properties: {
+            name: {isVisible: alwaysVisible},
+            location: {isVisible: visibleOnlyInDetail},
+            email: {isVisible: alwaysVisible},
+            number: {isVisible: alwaysVisible},
+            area: {isVisible: alwaysVisible},
+            costPerHour: {isVisible: visibleOnlyInDetail},
+            open: {isVisible: alwaysVisible},
+            rating: {isVisible: visibleOnlyInDetail},
+            hasCCTV: {isVisible: visibleOnlyInDetail},
+            address: {isVisible: visibleOnlyInDetail},
+            longAddress: {isVisible: visibleOnlyInDetail},
+            ownerName: {isVisible: visibleOnlyInDetail},
+            ownerDetail: {isVisible: visibleOnlyInDetail},
+            openingTime: {isVisible: visibleOnlyInDetail},
+            closingTime: {isVisible: visibleOnlyInDetail},
+            timings: {isVisible: visibleOnlyInDetail}
+
+        }
+    }
+}
+
 const adminBro = new AdminBro({
-    resources: [User, Area, Booking, StorageSpace, Transaction, Image],
+    resources: [userResource, Area, Booking, storageSpaceResource, Transaction],
     rootPath: '/admin',
     branding: {
         companyName: 'GoLuggageFree',
