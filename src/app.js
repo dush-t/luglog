1c39c491754c52ab6d6d36233270fc7be3fcbb73
Dushyant Yadav <dushyant9309@gmail.com>
2019-10-28 13:02:05 +0530

    Implement admin panel

--- src/app.js
+++ src/app.js
@@ -2,22 +2,25 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require('path');
+const hbs = require('hbs')
+const AdminBro = require('admin-bro');
+const AdminBroExpressjs = require('admin-bro-expressjs');
 
-// Setting up Sentry
+// SETUP SENTRY
 const Sentry = require('@sentry/node');
 Sentry.init({ dsn: process.env.SENTRY_DSN })
 app.use(Sentry.Handlers.requestHandler());
 
-const bodyParser = require('body-parser');
-// const multer = require('multer');
-const path = require('path');
-const hbs = require('hbs')
 
-// const upload = multer();
+const upload = multer();
 
-// Setting up database
-require('./db/mongoose'); // calling require will ensure that the file runs.
+// OPEN DATABASE CONNECTION
+require('./db/mongoose');
 
+// IMPORT ROUTERS
 const userRouter = require('./routers/user');
 const storageSpaceRouter = require('./routers/storageSpace');
 const bookingRouter = require('./routers/booking');
@@ -26,7 +29,25 @@ const imageRouter = require('./routers/image');
 const transactionRouter = require('./routers/transaction');
 
 
-// log all requests to terminal, just like django.
+// SETUP ADMIN PANEL
+AdminBro.registerAdapter(require('admin-bro-mongoose'))
+const adminBro = new AdminBro({
+    resources: [User, Market, Stock, StockTransaction],
+    rootPath: '/admin',
+})
+const router = AdminBroExpressjs.buildAuthenticatedRouter(adminBro, {
+    authenticate: (email, password) => {
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            return {email: process.env.ADMIN_EMAIL, password: process.env.ADMIN_PASSWORD};
+        } else {
+            return null;
+        }
+    },
+    cookiePassword: process.env.ADMIN_COOKIE_PASSWORD
+})
+
+
+// SETUP LOGGING MIDDLEWARE
 const loggerMiddleware = (req, res, next) => {
     console.log(req.method + ' ' + req.path);
     next();
@@ -37,11 +58,15 @@ const viewsPath = path.join(__dirname, '../templates/views');
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
 
-app.use(express.json()); // ask express to automatically parse incoming json.
+// SETUP REQUEST-PARSING MIDDLEWARE
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(upload.array()); 
+app.use(upload.array()); 
 
 app.use(loggerMiddleware);
+
+// USE ADMIN-PANEL
+app.use(adminBro.options.rootPath, router)
 
 app.use(userRouter);
 app.use(areaRouter);
--- src/routers/booking.js
+++ src/routers/booking.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const sharp = require('sharp');
+
 const StorageSpace = require('../models/storageSpace');
 const Booking = require('../models/booking');
 const User = require('../models/user');
 
-const { sendBookingEmailToSpace, sendBookingEmailToUser } = require('../utils/email');
-const { sendSMS } = require('../utils/sms')
 const { generateBookingId } = require('../utils/randomString');
 const { getDays } = require('../utils/dateTime');
 
@@ -42,13 +40,6 @@ router.post('/api/bookings/:space_id/book', auth, async (req, res) => {
     req.user.is_luggage_stored = true;
     req.user.bookings.push(booking._id);
     await req.user.save();
-
-    // const smsBody = `You have a new booking from ${req.user.name}. The number of bags is ${booking.numberOfBags} and the booking is for ${booking.numberOfDays} days. Booking ID: ${booking.bookingId}`
-
-    // sendSMS(storageSpace.number, smsBody);
-
-    // sendBookingEmailToSpace(storageSpace.email, storageSpace.name);
-    // sendBookingEmailToUser(req.user.email, req.user.name, storageSpace.name);
 
     return res.status(201).send(booking);
 })
@@ -113,15 +104,5 @@ router.get('/api/booking/:booking_id', auth, async (req, res) => {
 
     res.status(200).send(booking);
 })
-
-
-// //UPLOAD IMAGES FOR BOOKING
-// router.post('/api/bookings/:booking_id/uploadImages', auth, imageUpload.single('luggageItem'), async (req, res) => {
-//     let booking = await Booking.findById(req.params.booking_id);
-//     const buffer = await WaveShaperNode(req.file.buffer).resize({ width: 300, height: 300 }).png().toBuffer();
-//     booking.luggageItems.push({ photo: buffer, totalStorageCost: 0 });
-//     await booking.save()
-// })
-
 
 module.exports = router;
