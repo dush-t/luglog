4d38358db466bb9686c9dd3d7ce9a65b3961c68a
Dushyant Yadav <dushyant9309@gmail.com>
2019-11-11 21:34:35 +0530

    bugfix in storageSpaceResolver

--- src/graphql/resolvers/storageSpaceResolver.js
+++ src/graphql/resolvers/storageSpaceResolver.js
@@ -6,16 +6,16 @@ const resolver = {
     Query: {
         async storageSpace(parent, args) {
             if (parent._id) {
-                const storageSpace = await StorageSpace.findOne({ _id: parent.storageSpace }).populate('area').execPopulate();
+                const storageSpace = await StorageSpace.findOne({ _id: parent.storageSpace }).populate('area');
                 return storageSpace;
             } else {
-                const storageSpace = await StorageSpace.findOne({ _id: args._id }).populate('area').execPopulate();
+                const storageSpace = await StorageSpace.findOne({ _id: args._id }).populate('area');
                 return storageSpace;
             }
         },
 
         async storageSpaces(parent, args) {
-            const storageSpaces = await StorageSpace.find(args).populate('area').execPopulate();
+            const storageSpaces = await StorageSpace.find(args).populate('area');
             return storageSpaces;
         }
     }
