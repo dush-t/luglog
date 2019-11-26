ee1794e2c39cfd8926fb60f17bdae8a0f0abe254
Dushyant Yadav <dushyant9309@gmail.com>
2019-11-26 18:33:57 +0530

    Auto-reconnect if database connection fails during runtime, undo last commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,16 +1,8 @@
 const mongoose = require('mongoose');
 
-const connectWithRetry = () => {
-    try {
-        return mongoose.connect(process.env.MONGODB_URL, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-            autoReconnect: true
-        });
-    } catch (e) {
-        setTimeout(connectWithRetry, 5000)
-    }
-}
-
-connectWithRetry();
+mongoose.connect(process.env.MONGODB_URL || "mongodb://luglogadmin:lolmao12345@mongo0:27017,mongo1:27018,mongo2:27019/luglog?authSource=admin", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    autoReconnect: true
+});
