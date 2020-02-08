55f8c2f3cf37f531c742ca334bc67e960b768063
Dushyant Yadav <dushyant9309@gmail.com>
2020-02-08 21:07:58 +0530

    Temporary fix for huge problem

--- src/constants/couponConstants.js
+++ src/constants/couponConstants.js
@@ -1,5 +1,5 @@
 const referralDiscountConstants = {
-    VALUE: 20,
+    VALUE: 30,
     CONSTRAINTS: '{}',
     TITLE: '20% off',
     DESCRIPTION: 'Refer GoLuggageFree to your friends and earn 20% off on your next booking'
